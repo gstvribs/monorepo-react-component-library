18efff7bb9f7228c60cfc77fb81c141f23035642
Gustavo Ribeiro <gu_ribeiro@live.com>
2020-11-19 23:43:09 -0300

    chore: update jest configs and test environments
    Signed-off-by: Gustavo Ribeiro <gu_ribeiro@live.com>

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,25 @@
 module.exports = {
-  roots: ['src'],
-  setupFilesAfterEnv: ['./jest.setup.ts'],
+  roots: ['<rootDir>/components'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js'],
+  testMatch: ['<rootDir>/components/**/*.test.tsx'],
+  modulePaths: ['<rootDir>/dist'],
   testPathIgnorePatterns: ['node_modules/'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  testMatch: ['**/*.test.(ts|tsx)'],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       'identity-obj-proxy',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
+  coverageReporters: [
+    [
+      'json',
+      {
+        projectRoot: '../../',
+      },
+    ],
+    'text',
+  ],
 };
